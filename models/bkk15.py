70224092c6ecf242c4865c1758ce40dab479289e
dongwooc <dongwooc@stanford.edu>
2015-11-13 08:41:09 -0800

    implemented fduty as a function of redshift

--- models/bkk15.py
+++ models/bkk15.py
@@ -4,11 +4,13 @@
     for simplicity's sake, we will use the end result relation (2.6)
 """
 
-import os
 import numpy as np
-import scipy.interpolate
+import scipy.integrate as spint
+import scipy.interpolate as interp
+from astropy.cosmology import WMAP9
+import astropy.units
 
-def line_luminosity(halos, line_freq, A=2e-6, b=1., fduty=0.1, min_mass=1e9):
+def line_luminosity(halos, line_freq, A=2e-6, b=1., min_mass=1e9, cosmo=WMAP9):
     """
     Parameters
     halos : HaloList object
@@ -22,6 +24,14 @@ def line_luminosity(halos, line_freq, A=2e-6, b=1., fduty=0.1, min_mass=1e9):
     """
     hm          = halos.m
     hz          = halos.zcos
+    
+    # lazy fduty calculation
+    Omega_m,Omega_k,Omega_Lambda = cosmo.Om0,cosmo.Ok0,cosmo.Ode0
+    Omega_rad = 1-Omega_m-Omega_k-Omega_Lambda
+    lookback_integrand = lambda z:(Omega_rad*(1+z)**4+Omega_m*(1+z)**3+Omega_k*(1+z)**2+Omega_Lambda)**(-0.5)/(1+z)
+    age_func = np.vectorize(lambda z:spint.quad(lookback_integrand,z,np.inf)[0])
+    h_age = cosmo._hubble_time.to(astropy.units.Gyr).value*age_func(hz)
+    fduty       = 0.1/h_age
 
     lco = np.where(
             hm >= min_mass, 
